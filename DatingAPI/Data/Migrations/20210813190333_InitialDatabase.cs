620e097412beb1f5f529cafd8d87fd74f9be08ec
claude cavour <aristide22lapo@gmail.com>
2021-08-13 23:05:52 +0200

    My first commit

---
+++ DatingAPI/Data/Migrations/20210813190333_InitialDatabase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingAPI.Data.Migrations
+{
+    public partial class InitialDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder
+                   .Sql("INSERT INTO Users (UserName) Values ('Claude')");
+            migrationBuilder
+                  .Sql("INSERT INTO Users (UserName) Values ('Cavour')");
+            migrationBuilder
+                  .Sql("INSERT INTO Users (UserName) Values ('Myriam')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder
+              .Sql("DELETE FROM Users");
+        }
+    }
+}
